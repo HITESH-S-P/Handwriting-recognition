1b7b65a3ff40354f72010601dba7b4a2905e6e6e
HITESH S P <hiteshsp2004@gmail.com>
2026-01-31 19:55:57 +0530

    pagplab

--- fake.py
+++ fake.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 # Replace with your actual key or set environment variable
-genai.configure(api_key="AIzaSyAQDb0fvkX0SUxPJ-uNfOfIEUL8gPONp8E")
+genai.configure(api_key="API_KEY")
 
 print("Available models:")
 for m in genai.list_models():
